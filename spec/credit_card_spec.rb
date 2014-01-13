ab09c4bc5921e805f0a0d93d95d30b9b1389a6c4
Jordan Morano <jordan.morano@gmail.com>
2014-01-13 09:40:53 -0500

    wow - missing an end there!

--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -27,8 +27,9 @@ describe CreditCard do
     it 'returns false => name is not valid first and last' do
       my_cc = CreditCard.new('375024253860990', '11/19', '123', 'Jordan', '02139')
       expect(my_cc.valid?). to eq false
+    end
 
-      it 'returns false => billing zip is not 5 digits' do
+    it 'returns false => billing zip is not 5 digits' do
       my_cc = CreditCard.new('375024253860990', '11/19', '123', 'Jordan Morano', '0213')
       expect(my_cc.valid?). to eq false
     end
