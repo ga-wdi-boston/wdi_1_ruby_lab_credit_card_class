af7899fa5eb79528bea8321b15e72d084103a7d1
Laure Fischer <laurefischer27@gmail.com>
2014-01-13 09:53:22 -0500

    fixed typo

--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -5,14 +5,14 @@ require_relative '../lib/credit_card'
 describe CreditCard do
   describe '#initialize' do
     it 'has an initialize method' do
-      expect (CreditCard.new).to eq CreditCard.initialize
+      expect(CreditCard.new("Laure Fischer")).to eq CreditCard.initialize("Laure Fischer")
     end
   end
 
   describe '#valid?' do
     it 'checks that the card has a name' do
       new_card = CreditCard.new("Laure Fischer")
-      expect (new_card.valid?).to eq true
+      expect(new_card.valid?).to eq true
     end
   end
 end
