ccb40680f56f98bf7c7cd9c1e6e0ea6ee739f459
Ashlinn Brennan <asbren13@gmail.com>
2015-01-15 23:23:54 -0500

    lab almost complete. missing luhn test

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,2 +1,89 @@
-class CreditCard
+require 'date'
+
+class InvalidExpDate < ArgumentError
 end
+
+class CreditCard
+
+  def initialize(number, exp_date, ccv, name, zip)
+    @number = number
+    @exp_date = exp_date.to_s
+    @ccv = ccv.to_s
+    @name = name
+    @zip = zip.to_s
+  end
+
+  def number
+    @number
+  end
+
+  def name
+    @name
+  end
+
+  def zip
+    @zip
+  end
+
+  def exp_date
+    @exp_date
+  end
+
+  def valid_cc_number?
+    @number.to_s.length == 13 ||
+    @number.to_s.length == 15 ||
+    @number.to_s.length == 16
+  end
+
+  def valid_zip?
+    @zip.length == 5
+  end
+
+  def valid_name?
+    return true if defined? @name
+  end
+
+  def valid_exp_date?
+    expiration_date = Date.parse(@exp_date)
+    today = DateTime.now
+    (expiration_date - today) > 0
+  end
+
+
+
+  def card_type
+    if number.to_s[0] == "4" &&
+      (number.to_s.length == 13 ||
+       number.to_s.length == 16)
+      card_type = "Visa" #works
+    elsif number.to_s[0] == "3" &&
+        (number.to_s[1] == "4" ||
+        number.to_s[1] == "7") &&
+        number.to_s.length == 15
+        card_type =  "AmEx" #works
+    elsif number.to_s[0] == "5" &&
+       (number.to_s[1].to_i >= 1 &&
+       number.to_s[1].to_i <= 5) &&
+       number.to_s.length == 16
+       card_type =  "MasterCard" #works
+    elsif (number.to_s[0..7].to_i >= 60110000 &&
+      number.to_s[0..7].to_i <= 60119999) ||
+      (number.to_s[0..7].to_i >= 65000000 &&
+      number.to_s[0..7].to_i <= 65999999) ||
+      (number.to_s[0..7].to_i >= 62212600 &&
+      number.to_s[0..7].to_i <= 62292599)
+      number.to_s.length == 16
+      card_type =  "Discover" #works
+    else
+      return "Invalid Card"
+    end
+  end
+
+  def luhn_validity(number)
+    # I am still working to create a working solution of this. I know that essentially I need to double every other digit (and if the product of doubling those digits is greater than 9, I have to sum the digits of the products), then I need to sum all of the digits, and then figure out if the total modulo 10 is equal to 0 (if the total ends in zero)
+
+  end
+
+
+end
+
--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -1,1 +1,54 @@
+require 'date'
 require_relative '../lib/credit_card'
+
+RSpec.describe CreditCard do
+  let(:number){372542760020512}
+  let(:exp_date){"10/2016"} #I tried almost a hundred different ways to see if this could just be a 2 digit month and a 2 digit year, but haven't found a way yet, so I am leaving it as is
+  let(:ccv){987}
+  let(:name){"John Smith"}
+  let(:zip){11234}
+  subject(:credit_card){CreditCard.new(number, exp_date, ccv, name, zip)}
+
+  describe '#initialize' do
+    it 'creates a new instance of a Credit Card with all credit card information' do
+      expect(credit_card).to be_a CreditCard
+    end
+  end
+
+  describe '#valid cc number' do
+    it 'checks that cc number has 13-16 digits' do
+      expect(credit_card.valid_cc_number?).to be true
+    end
+  end
+
+  describe '#valid zip' do
+    it 'checks that the zip has 5 digits' do
+      expect(credit_card.valid_zip?).to be true
+    end
+  end
+
+  describe '#valid name' do
+    it 'checks that the name is defined' do
+      expect(credit_card.valid_name?).to be true
+    end
+  end
+
+  describe '#valid_exp_date' do
+    it 'checks that expiration date is valid' do
+      expect(credit_card.valid_exp_date?).to be true
+    end
+  end
+
+  describe '#card_type' do
+    it 'checks that card is a valid card type' do
+      expect(credit_card.card_type).to eq "AmEx"
+    end
+  end
+
+  # describe '#luhn_validity' do
+  #   it 'checks if credit card is valid' do
+  #     expect(credit_card.luhn_validity).to be true
+  #   end
+  # end
+
+end
