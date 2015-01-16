19cec185db9ddae1283fe9d8a607ae08a7220bcf
lady3bean <annehawleybrett@gmail.com>
2015-01-15 22:39:33 -0500

    adds the methods for credit cards

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,2 +1,44 @@
+
+
 class CreditCard
+  
+  def initialize(name, number, expiration, ccv, zip)
+    @name = name
+    @number = number
+    @expiration = expiration
+    @ccv = ccv
+    @zip = zip
+  end
+
+  def name
+    @name
+  end
+
+  def number
+    @number
+  end
+
+  def valid_number
+    if number.length == 16
+      return true
+    else
+      return false
+    end
+  end
+
+  def expiration
+    @expiration
+  end
+
+  def ccv
+    @ccv
+  end
+
+  def zip
+    @zip
+  end
+  
+
+  #def valid?  checks the validity of the credit card. Check that it has a name, proper length zip code, 16-digit number and an expiration date in the future.
 end
+
--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -1,1 +1,56 @@
 require_relative '../lib/credit_card'
+
+RSpec.describe CreditCard do
+
+  let(:name) {"john"}
+  let(:number) {"12341283412341234"}
+  let(:expiration) {"Date"} # look up Date class and parsing
+  let(:ccv) {"987"}
+  let(:zip) {"02158"}
+  subject(:creditcard) {CreditCard.new(name, number, expiration, ccv, zip)}
+
+  describe '#initialize' do
+    it 'creates a new instance of CreditCard with a name and a number' do
+      expect(creditcard).to be_a CreditCard
+    end
+  end
+
+  describe '#name' do
+    it 'returns the name of the CreditCard' do
+      expect(creditcard.name).to be name
+    end
+  end
+
+  describe '#number' do
+    it 'returns the number of the CreditCard' do
+      expect(creditcard.valid_number).to be true
+    end
+  end
+
+  describe '#expiration' do
+    it 'returns the expiration of the CreditCard' do
+      expect(creditcard.expiration).to be expiration
+    end
+  end
+
+  describe '#ccv' do
+    it 'returns the ccv of the CreditCard' do
+      expect(creditcard.ccv).to be ccv
+    end
+  end
+
+  describe '#zip' do
+    it 'returns the zip of the CreditCard' do
+      expect(creditcard.zip).to be zip
+    end
+  end
+
+end
+
+
+
+# Credit card # (16-digits)
+# - Expiration Date (two digit month, two digit year)
+# - CCV (3 digit number on the back)
+# - Name
+# - Billing Zip Code (5-digits)
