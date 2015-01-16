cbdfd58670de9122456770c46075f93ea87f6288
idelairre <idelairre@gmail.com>
2015-01-15 21:51:37 -0500

    Writes tests for the credit card problem. Defines methods for all but the "luhn" evaluation.

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,2 +1,62 @@
+require 'pry'
+
 class CreditCard
+  def initialize(card_number, exp_date, ccv, name, zip)
+    @card_number = card_number
+    @exp_date = exp_date
+    @ccv = ccv
+    @name = name
+    @zip = zip
+  end
+
+  # def valid_num?
+  #   card_number_array = @card_number.chars.reverse
+  #   if card_number_array.length != 16
+  #     return false
+  #   end
+  # end
+
+#Reverse the digits in the credit card number
+#pick the odd digits out of the reversed credit card number and put them in an array
+#sum the resultant 'odd' digits and save that value as =sum1
+#pick the even digits out of the reversed credit card number and put them in an arry
+#multiple each of the digits in the even_digits array *2
+#convert this array into characters and then into an array of indv_even_digits
+#sum all the digits in the array indv_even_digits and save that value as =sum2
+#if sum1 + sum2 = 70, you have passed the Luhn test
+
+
+    # int_ary = card_number_array.map { |x| x.to_i }
+    #   card_number_array.each_index do |x|
+    #     card_number_array[x] = card_number_array[x].to_i
+    #     if x % 2 != 0
+    #       card_number_array[x] * 2
+    #       if card_number_array[x] > 9
+    #         card_number_array[x] - 9
+    #       end
+    #     end
+    #  (card_number_array.reduce(:+) + last_digit) % 10 == 0
+    #   end
+
+
+  def valid_zip?
+    @zip.length == 5
+  end
+
+  def valid_exp?
+    @exp_date - Time.now > 0
+  end
+
+  def card_type?
+    card_type = "Other"
+     if @card_number.slice(0..1).eql?("34") || @card_number.slice(0..1).eql?("27")
+       card_type = "American Express"
+     elsif @card_number[0].eql?("4")
+      card_type = "Visa"
+     elsif @card_number.slice(0..3).eql?("6101")
+       card_type = "Discover"
+     elsif @card_number.slice(0..1).eql?("51") || @card_number.slice(0..1).eql?("52") || @card_number.slice(0..1).eql?("53") || @card_number.slice(0..1).eql?("54") || @card_number.slice(0..1).eql?("55")
+       card_type = "Master Card"
+      end
+  end
 end
--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -1,1 +1,55 @@
 require_relative '../lib/credit_card'
+
+  RSpec.describe CreditCard do
+    let(:card_number) { "4539650579244728" }
+    let(:exp_date) { Time.new(2015, 3) }
+    let(:ccv) { 452 }
+    let(:name) { "Broey Broerson"}
+    let(:zip) { "12321" }
+
+    subject(:credit_card) { CreditCard.new(card_number, exp_date, ccv, name, zip)}
+    describe '#initialize' do
+      it "it creates a new instance of creditcard" do
+      expect(credit_card).to be_a CreditCard
+    end
+  end
+
+  describe '#valid_num?' do
+    it 'determines whether or not the card is valid' do
+      expect(credit_card.valid_num?).to be true
+    end
+  end
+
+  describe '#card_type?' do
+    it 'determines the service provider of the card' do
+      expect(credit_card.card_type?).to eq "Visa"
+    end
+  end
+
+  describe '#valid_zip?' do
+    it 'determines whether the zip code is valid or not' do
+      expect(credit_card.valid_zip?).to be true
+    end
+  end
+
+  describe '#valid_exp?' do
+    it 'determines whether the expiration date is valid or not' do
+      expect(credit_card.valid_exp?).to be true
+    end
+  end
+end
+
+    RSpec.describe CreditCard do
+    let(:card_number) { "6101650579244728" }
+    let(:exp_date) { Time.new(3, 15) }
+    let(:ccv) { 452 }
+    let(:name) { "Testy McAss"}
+    let(:zip) {1232}
+
+    subject(:credit_card) { CreditCard.new(card_number, exp_date, ccv, name, zip)}
+  describe '#card_type?' do
+    it 'determines the service provider of the card' do
+      expect(credit_card.card_type?).to eq "Discover"
+    end
+  end
+end
