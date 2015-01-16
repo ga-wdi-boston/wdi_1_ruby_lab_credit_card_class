599e4a70d715084aebd67b7019d41e81c5e74ec0
Helen Thi <ht5658@gmail.com>
2015-01-15 21:42:32 -0500

    Credit card assessment

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,2 +1,95 @@
+require 'date'
+
 class CreditCard
+  def initialize(card_number, exp_date, ccv, name, zip_code)
+    @card_number = card_number
+    @exp_date = exp_date
+    @ccv = ccv
+    @name = name
+    @zip_code = zip_code
+  end
+
+  def luhn
+    card_num_array = @card_number.to_s.chars
+
+    card_num_array.map do |x|
+      if x.to_i%2 != 0
+        product = x.to_i * 2
+          if product > 9
+            card_sum_array = product.to_s.chars
+            card_sum_array.map do |y|
+              y.to_i
+            end.inject(:+)
+          else
+            product
+          end
+      else
+          x.to_i
+      end
+    end.inject(:+)
+
+  end
+
+#returns true if expired
+  def expired?
+    Date.new(@exp_date) - Date.today < 0
+  end
+
+  def card_type
+    # American Express. First digit must be a 3 and second digit must be a 4 or 7. Valid length: 15 digits.
+    if @card_number.to_s[0] = "3" &&
+      (@card_number.to_s[1].to_s = "4" ||
+      @card_number.to_s[1].to_s = "7") &&
+      @card_number.to_s.length == 15
+      card_type = "American Express"
+    # Mastercard/First digit must be a 5 and second digit must be in the range 1 through 5 inclusive
+    elsif @card_number.to_s[0] = "5" &&
+      (@card_number.to_s[1] >= 1 &&
+      @card_number.to_s[1] <= 5) &&
+      @card_number.to_s.length == 16
+      card_type = "Mastercard"
+    # Visa/First digit must be a 4. Valid length: 13 or 16 digits.
+    elsif @card_number.to_s[0] = "4" &&
+      (@card_number.to_s.length == 13 ||
+      @card_number.to_s.length == 16)
+      card_type = "Visa"
+    # Discover/ Valid length: 16 digits. First 8 digits must be in one of the following ranges:60110000 through 60119999, 65000000 through 65999999, 62212600 through 62292599
+    elsif
+      (@card_number.to_s[0..7].to_i >= 60110000 &&
+      @card_number.to_s[0..7].to_i <= 60119999) ||
+
+      (@card_number.to_s[0..7].to_i >= 65000000 &&
+      @card_number.to_s[0..7].to_i <= 65999999) ||
+
+      (@card_number.to_s[0..7].to_i >= 62212600 &&
+      @card_number.to_s[0..7].to_i <= 62292599)
+
+      @card_number.to_s.length == 16
+      card_type = "Discover"
+    else
+      card_type = "Other"
+    end
+  end
+
+  def valid?
+    if @card_number.to_s.length == 15
+      card_type = "American Express"
+    elsif @card_number.to_s.length == 16
+      card_type = "Mastercard"
+    elsif (@card_number.to_s.length == 13 ||
+      @card_number.to_s.length == 16)
+      card_type = "Visa"
+    elsif @card_number.to_s.length == 16
+      card_type = "Discover"
+    else
+      card_type = "Other"
+   end &&
+    @name != "" &&
+    @zip_code.to_s.length == 5 &&
+    expired? &&
+    luhn%10 == 0
+  end
+
 end
+
+
--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -1,1 +1,82 @@
 require_relative '../lib/credit_card'
+
+RSpec.describe CreditCard do
+
+  let(:card_number) { 9234567890123456 }
+  let(:exp_date) { 1/15/2016 }
+  let(:ccv) { 123 }
+  let(:name) { "Pat Needham" }
+  let(:zip_code) { 12345 }
+
+  subject(:credit_card) { CreditCard.new(card_number, exp_date, ccv, name, zip_code) }
+
+  describe '#initialize' do
+    it 'creates a new instance of a credit card with attributes' do
+      expect(credit_card).to be_a CreditCard
+    end
+  end
+
+  #credit card has 16 digits---although valid method will allow exceptions to this depending on what American Express, Mastercard, Visa, and Discover allow
+   describe '#card number' do
+    it 'checks if credit card has 16 digits' do
+      expect(card_number.to_s.length).to eq 16
+    end
+  end
+
+  #ccv has 3 digits
+  describe '#ccv' do
+    it 'checks if ccv has 3 digits' do
+      expect(ccv.to_s.length).to eq 3
+    end
+  end
+  #name was given
+  describe '#name' do
+    it 'checks if name is given' do
+      expect(name.length).not_to eql ""
+    end
+  end
+
+
+  #zip code is 5 digits
+  describe '#zip code?' do
+    it 'checks if zip code has 5 digits' do
+      expect(zip_code.to_s.length).to eq 5
+    end
+  end
+
+   #tests validity of card_number using luhn value
+  describe '#luhn%10' do
+    it 'checks validity of card_number using luhn value' do
+      expect(credit_card.luhn%10 == 0).to eq false
+    end
+  end
+
+  #tests if card is expired
+  describe '#expired?' do
+    it 'checks if card is expired' do
+      expect(credit_card.expired?).not_to eq false
+    end
+  end
+
+  #validates whether card number passes luhn test
+  #is valid
+  describe '#luhn' do
+    it 'checks with luhn test if card is valid' do
+      expect(credit_card.luhn).to eq 71
+    end
+  end
+
+  #validates whether card type is American Express, Mastercard, Visa, Discover, or Other
+  describe '#card_type' do
+    it 'checks whether the card type is American Express, Mastercard, Visa, Discover, or Other' do
+      expect(credit_card.card_type).to eq "Other"
+    end
+  end
+
+   describe '#valid?' do
+    it 'checks if card is ultimately valid' do
+      expect(credit_card.valid?).to eq false
+    end
+  end
+
+end
