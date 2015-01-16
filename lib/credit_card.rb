76c78a2856a49b51179ff242241194e3855ef433
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-01-16 07:49:53 -0500

    Create tests and second two methods

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,2 +1,22 @@
 class CreditCard
+
+  def initialize(number, expiration_date, ccv, name, zip)
+    @number = number
+    @exp_date = expiration_date
+    @ccv = ccv
+    @name = name
+    @zip = zip
+  end
+
+  def has_name?(name) # the return for this needs to be true
+    # does my card has a name
+    # yes == true
+    # no == false
+  end
+  def zip_length(zip)
+
+   @zip.length.to_i == 5
+   true
+
+  end
 end
