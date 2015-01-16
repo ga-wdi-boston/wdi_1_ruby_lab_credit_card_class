c2144584fdb73d6990d0492bad1b047060a846e3
katyf <katy.feng.14@gmail.com>
2015-01-15 21:14:42 -0500

    Credit card lab - finished.

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,2 +1,74 @@
+require 'date'
+require 'byebug'
 class CreditCard
+  def initialize (number, exp_month, exp_year, ccv, name, zip_code)
+    @number = number
+    @exp_month = exp_month
+    @exp_year = exp_year
+    @ccv = ccv
+    @name = name
+    @zip_code = zip_code
+  end
+
+  def number
+    @number = number
+  end
+
+  def exp_month
+    @exp_month = exp_month
+  end
+
+  def exp_year
+    @exp_year = exp_year
+  end
+
+  def ccv
+    @ccv = ccv
+  end
+
+  def name
+    @name = name
+  end
+
+  def zip_code
+    @zip_code = zip_code
+  end
+
+  def is_valid
+    @number.to_s.length == 16 && @zip_code.to_s.length == 5 && @name != nil
+  end
+
+  def date_is_valid
+    if @exp_year == Date.today.strftime("%y").to_i
+         @exp_month > Date.today.strftime("%m").to_i
+    else
+         @exp_year > Date.today.strftime("%y").to_i
+    end
+  end
+
+  def luhn_validity
+    digits = @number.to_s.scan(/./).map(&:to_i)
+    check = digits.pop
+    sum = digits.reverse.each_slice(2).map do |x, y|
+      [(x * 2).divmod(10), y || 0]
+    end.flatten.inject(:+)
+    luhn_validity =(10 - sum % 10) == check
+    luhn_validity
+  end
+
+  def card_type
+    if @number.to_s.split("")[0] == "4"
+      card_type = "Visa"
+    elsif @number.to_s.split("")[0] == "5"
+      card_type = "Mastercard"
+    elsif @number.to_s.split("")[0] == "6"
+      card_type = "Discover Card"
+    elsif @number.to_s.split("")[0] == "3"
+      card_type = "American Express"
+    else
+      card_type = "Unknown Card Type"
+    end
+    card_type
+  end
+
 end
--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -1,1 +1,44 @@
 require_relative '../lib/credit_card'
+require 'spec_helper'
+
+RSpec.describe CreditCard do
+
+  let(:number) { 4532792914262853 }
+  let(:exp_month) { 10 }
+  let(:exp_year) { 17 }
+  let(:ccv) { 123 }
+  let(:name) { "John Smith" }
+  let(:zip_code) { 12345 }
+  subject(:credit_card) { CreditCard.new(number, exp_month, exp_year, ccv, name, zip_code) }
+
+  describe '#initialize' do
+    it 'Creates a new credit card' do
+      expect(credit_card).to be_a CreditCard
+    end
+  end
+
+  describe '#is_valid' do
+    it 'Returns if the credit card information is valid' do
+      expect(credit_card.is_valid).to be true
+    end
+  end
+
+  describe '#date_is_valid' do
+    it 'Returns if the expiration date is valid' do
+      expect(credit_card.date_is_valid).to be true
+    end
+  end
+
+  describe '#luhn_validity' do
+    it 'Returns if the card number is valid' do
+      expect(credit_card.luhn_validity).to be true
+    end
+  end
+
+  describe '#card_type' do
+    it 'Returns the card card type.' do
+      expect(credit_card.card_type).to eq "Visa"
+    end
+  end
+
+end
