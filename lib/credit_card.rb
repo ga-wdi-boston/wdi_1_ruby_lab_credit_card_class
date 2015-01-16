774149654fd6ade6b9e5bf4731d72b1d8cac3e69
Dan Stoppelman <dstop75@gmail.com>
2015-01-15 22:30:02 -0500

    Adding initialize method

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,2 +1,9 @@
 class CreditCard
+  def initialize(name, zip, card_number, expiration, ccv)
+    @name = name
+    @zip = zip
+    @card_number = card_number
+    @expiration = expiration
+    @ccv = ccv
+  end
 end
