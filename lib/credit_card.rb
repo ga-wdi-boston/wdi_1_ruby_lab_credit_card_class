0023d931407c7a4811cad6df306fae493c6e619a
idelairre <idelairre@gmail.com>
2015-01-17 12:32:19 -0500

    finally got everything to work after a poop-ton of work and not borrowing any code (hell yeah).

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,43 +1,49 @@
 require 'pry'
 
 class CreditCard
-  def initialize(card_number, exp_date, ccv, name, zip)
+  def initialize(card_number, exp_date, cvv, name, zip)
     @card_number = card_number
     @exp_date = exp_date
-    @ccv = ccv
+    @cvv = cvv
     @name = name
     @zip = zip
   end
 
-  # def valid_num?
-  #   card_number_array = @card_number.chars.reverse
-  #   if card_number_array.length != 16
-  #     return false
-  #   end
-  # end
+  def valid_num?
+    binding.pry
+    #  if card_number_array.length != 16
+    #    return false
+    # else
 
-#Reverse the digits in the credit card number
-#pick the odd digits out of the reversed credit card number and put them in an array
-#sum the resultant 'odd' digits and save that value as =sum1
-#pick the even digits out of the reversed credit card number and put them in an arry
-#multiple each of the digits in the even_digits array *2
-#convert this array into characters and then into an array of indv_even_digits
-#sum all the digits in the array indv_even_digits and save that value as =sum2
-#if sum1 + sum2 = 70, you have passed the Luhn test
+    #  end
+
+    card_number_array = @card_number.chars
+    last_digit = card_number_array.pop.to_i
 
 
-    # int_ary = card_number_array.map { |x| x.to_i }
-    #   card_number_array.each_index do |x|
-    #     card_number_array[x] = card_number_array[x].to_i
-    #     if x % 2 != 0
-    #       card_number_array[x] * 2
-    #       if card_number_array[x] > 9
-    #         card_number_array[x] - 9
-    #       end
-    #     end
-    #  (card_number_array.reduce(:+) + last_digit) % 10 == 0
-    #   end
+    int_array = card_number_array.reverse.map { |integer| integer.to_i }
 
+    int_array_odd = int_array.select.each_with_index { |integer, index| index.odd? }
+    int_array_even = int_array.select.each_with_index { |integer, index| index.even? }
+
+    int_array_odd.map! { |integer| integer * 2 }
+
+    int_array_odd.map! do |integer|
+    	if integer > 9
+    		integer - 9
+    	else
+    		integer
+    	end
+    end
+
+    sum = int_array_odd.reduce(:+) + int_array_even.reduce(:+)
+
+    if sum % 10 == last_digit
+      return true
+    else
+      return false
+    end
+  end
 
   def valid_zip?
     @zip.length == 5
@@ -49,14 +55,14 @@ class CreditCard
 
   def card_type?
     card_type = "Other"
-     if @card_number.slice(0..1).eql?("34") || @card_number.slice(0..1).eql?("27")
-       card_type = "American Express"
-     elsif @card_number[0].eql?("4")
+    if @card_number.slice(0..1).eql?("34") || @card_number.slice(0..1).eql?("27")
+      card_type = "American Express"
+    elsif @card_number[0].eql?("4")
       card_type = "Visa"
-     elsif @card_number.slice(0..3).eql?("6101")
-       card_type = "Discover"
-     elsif @card_number.slice(0..1).eql?("51") || @card_number.slice(0..1).eql?("52") || @card_number.slice(0..1).eql?("53") || @card_number.slice(0..1).eql?("54") || @card_number.slice(0..1).eql?("55")
-       card_type = "Master Card"
-      end
+    elsif @card_number.slice(0..3).eql?("6101")
+      card_type = "Discover"
+    elsif @card_number.slice(0..1).eql?("51") || @card_number.slice(0..1).eql?("52") || @card_number.slice(0..1).eql?("53") || @card_number.slice(0..1).eql?("54") || @card_number.slice(0..1).eql?("55")
+      card_type = "Master Card"
+    end
   end
 end
--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -1,13 +1,13 @@
 require_relative '../lib/credit_card'
 
   RSpec.describe CreditCard do
-    let(:card_number) { "4539650579244728" }
+    let(:card_number) { "4929767075986897" }
     let(:exp_date) { Time.new(2015, 3) }
-    let(:ccv) { 452 }
+    let(:cvv) { 452 }
     let(:name) { "Broey Broerson"}
     let(:zip) { "12321" }
 
-    subject(:credit_card) { CreditCard.new(card_number, exp_date, ccv, name, zip)}
+    subject(:credit_card) { CreditCard.new(card_number, exp_date, cvv, name, zip)}
     describe '#initialize' do
       it "it creates a new instance of creditcard" do
       expect(credit_card).to be_a CreditCard
@@ -15,7 +15,7 @@ require_relative '../lib/credit_card'
   end
 
   describe '#valid_num?' do
-    it 'determines whether or not the card is valid' do
+    it 'determines whether or not the card number is valid' do
       expect(credit_card.valid_num?).to be true
     end
   end
